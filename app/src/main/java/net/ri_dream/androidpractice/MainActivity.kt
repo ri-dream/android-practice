8712c144a54a2d4828af07b23b1497070796924f
k-hiyama <khiyama@ri-dream.net>
2022-02-22 12:06:09 +0900

    端末回転時の状態を保存する処理を追加. (02-03)

--- app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
+++ app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
@@ -2,6 +2,7 @@ package net.ri_dream.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -17,5 +18,18 @@ class MainActivity : AppCompatActivity() {
             val txtResult = findViewById<TextView>(R.id.txtResult)
             txtResult.text = Date().toString()
         }
+    }
+
+    // 画面が破棄される前に状態を保存
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        outState.putString("txtResult", txtResult.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = savedInstanceState.getString("txtResult")
     }
 }
