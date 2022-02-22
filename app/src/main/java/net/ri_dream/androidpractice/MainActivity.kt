bc655f957cf89f7a40b9563fbc71dd0d49c9e5c2
k-hiyama <khiyama@ri-dream.net>
2022-02-22 11:44:22 +0900

    onClickイベントをListenerとして定義するよう変更. (02-03)

--- app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
+++ app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
@@ -3,6 +3,7 @@ package net.ri_dream.androidpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
@@ -10,10 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(v: View) {
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
+        val btnCurrent = findViewById<Button>(R.id.btnCurrent)
+        btnCurrent.setOnClickListener {
+            val txtResult = findViewById<TextView>(R.id.txtResult)
+            txtResult.text = Date().toString()
+        }
     }
 }
