b0aa2b019fd6cee1e78e38bd59178db675a1624a
k-hiyama <khiyama@ri-dream.net>
2022-02-22 11:33:04 +0900

    Buttonクリックイベントで現在時刻を表示. (02-03)

--- app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
+++ app/src/main/java/net/ri_dream/androidpractice/MainActivity.kt
@@ -2,10 +2,18 @@ package net.ri_dream.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnCurrentClick(v: View) {
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = Date().toString()
     }
 }
